925cf166ef1f38ccd6cfec03810efffcebd92704
AtamaokaC <realatamaokac@gmail.com>
2020-02-24 23:52:55 +0900

    bitcount() added to mylib.cc

--- mylib.cc
+++ mylib.cc
@@ -598,6 +598,17 @@ for (auto i : range(10)) {
 }
 */
 
+int bitcount(long int a)
+{
+    a = (a & 0x55555555) + ((a & 0xAAAAAAAA) >> 1);
+    a = (a & 0x33333333) + ((a & 0xCCCCCCCC) >> 2);
+    a = (a & 0x0F0F0F0F) + ((a & 0xF0F0F0F0) >> 4);
+    a = (a & 0x00FF00FF) + ((a & 0xFF00FF00) >> 8);
+    a = (a & 0x0000FFFF) + ((a & 0xFFFF0000) >> 16);
+
+    return a;
+}
+
 using namespace std;
 typedef llint dtype;
 
