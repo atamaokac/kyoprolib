410c1d61820aedb139fdd49a6940f33f3608ce63
AtamaokaC <realatamaokac@gmail.com>
2020-02-24 23:52:16 +0900

    class range added to template.cc

--- template.cc
+++ template.cc
@@ -15,6 +15,28 @@ auto input(int N) {
     loop(i,N) std::cin >> v[i];
     return std::move(v);
 }
+class range {
+    typedef int Int;
+    Int start, stop, step;
+    void initialize() {
+        auto sign = (step >= 0) ? 1 : -1;
+        stop = start + ((stop-start-sign)/step + 1) * step;
+        if ((stop-start) * step <= 0) stop = start;
+    }
+public:
+    range(Int start, Int stop, Int step=1) : start(start), stop(stop), step(step) {
+        initialize();
+    }
+    range(Int stop) : start(0), stop(stop), step(1) { initialize(); }
+    struct iterator {
+        Int i, step;
+        Int& operator*() { return i; }
+        iterator& operator++() { i += step; return *this; }
+        bool operator!=(const iterator& v) { return i != v.i; }
+    };
+    iterator begin() { return {start, step}; }
+    iterator end()   { return {stop, step}; }
+};
 
 typedef llint Int;
 
